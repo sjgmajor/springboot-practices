17922490e68ea5de0c2806cbf8cd4f23044dc73d
서정권 <sjg3234@gmail.com>
2023-10-10 13:34:51 +0900

    springboot-helloworld #finish

--- springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.poscodx.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 1. Bootstraping class: 스프링 애플리케이션의 부트스트래칭 역할
@@ -15,14 +16,32 @@ public class HelloWorldApplication {
 		 * 
 		 * 1. 애플리케이션 컨텍스트(Application Context, Spring Container) 생성
 		 * 2. 어노테이션 스캐닝(Componet Scaning) + Configuration Class 를 통한
-		 * 	  빈 생성 및 등록 작업
+		 * 	  빈 생성, 등록, 와이어링 작업
 		 * 3. 만약, 웹애플리케이션 이라면 웹애플리케이션 타입을 결정(Spring MVC, Reactive)
 		 * 4. 만약, 웹애플리케이션이고 타입이 Spring MVC 이면
 		 * 	  - 내장(embeded) 서버(TomcatEmbedeServiceServletContainer) 인스턴스 생성
-		 * 	  - 서버 인스턴스 웹 애플리케이션 배포
+		 * 	  - 서버 인스턴스 웹 애플리케이션 배포(mvc, aop, security, jpa, ... auto Configuration)
 		 *    - 서버 인스턴스 스타트
 		 * 5. ApplicatinoRunner 인터페이스를 구현한 빈을 Application Context 찾아서 실행
 		 */
-		SpringApplication.run(HelloWorldApplication.class, args);
+//		ConfigurableApplicationContext ac = null;
+//		try {
+//		 ac = SpringApplication.run(HelloWorldApplication.class, args);
+//		} catch(Throwable ex) {
+//			ex.printStackTrace();
+//		} finally {
+//			if(ac != null) {
+//				ac.close();
+//			}
+//		}
+		
+		// try ~ with ~ resources 구문
+		try(ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args)){
+		
+			// web application인 경우
+			// SpringApplication.run(HelloWorldApplication.class, args);
+			
+			System.out.println("some code....");
+		}
 	}
 }
